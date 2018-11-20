1cd8f39eb2ffd70748cef61271839b23af478fef
David Vaquero <pepesan@gmail.com>
2018-11-20 09:12:23 +0100

    añadido ejemplo de one to many

---
+++ src/main/java/com/cursosdedesarrollo/hibernate/ejemplo03/AppMain03.java
@@ -0,0 +1,62 @@
+package com.cursosdedesarrollo.hibernate.ejemplo03;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class AppMain03 {
+
+    static Phone phone;
+    static Person person;
+    static Session sessionObj;
+    static SessionFactory sessionFactoryObj;
+
+    private static SessionFactory buildSessionFactory() {
+        // Creating Configuration Instance & Passing Hibernate Configuration File
+        Configuration configObj = new Configuration();
+        configObj.configure("hibernate03.cfg.xml");
+
+        // Since Hibernate Version 4.x, ServiceRegistry Is Being Used
+        ServiceRegistry serviceRegistryObj = new StandardServiceRegistryBuilder().applySettings(configObj.getProperties()).build();
+
+        // Creating Hibernate SessionFactory Instance
+        sessionFactoryObj = configObj.buildSessionFactory(serviceRegistryObj);
+        return sessionFactoryObj;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(".......Hibernate Maven Example.......\n");
+        try {
+            sessionObj = buildSessionFactory().openSession();
+            sessionObj.beginTransaction();
+
+
+            Person person = new Person();
+            Phone phone1 = new Phone( "123-456-7890" );
+            Phone phone2 = new Phone( "321-654-0987" );
+
+            person.getPhones().add( phone1 );
+            person.getPhones().add( phone2 );
+            sessionObj.persist( person );
+            sessionObj.flush();
+
+            person.getPhones().remove( phone1 );
+            System.out.println("\n.......Records Saved Successfully To The Database.......\n");
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                System.out.println("\n.......Transaction Is Being Rolled Back.......");
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+    }
+}
